2109aef03c694760ea744ef95ef042f2e29f08b1
Nikita Kiose <crotopus@yandex.ru>
2018-08-18 19:38:04 +0300

    First tests

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	public float moveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+
+		rb.velocity = new Vector2 (move * moveSpeed, rb.velocity.y);
+	}
+}
